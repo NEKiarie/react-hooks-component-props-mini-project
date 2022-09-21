77e27fe0214f0f06111da61c6d1456a86370165d
Elvis <s49188705@gmail.com>
2022-09-21 11:13:15 +0300

    Completed Assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  const postDetails = posts.map((post) => (
+    <Article
+      key={post.id}
+      title={post.title}
+      date={post.date}
+      preview={post.preview}
+      minutes={post.minutes}
+    />
+  ));
+
+  return (
+    <div>
+      <main>{postDetails}</main>
+    </div>
+  );
+}
+
+export default ArticleList;
